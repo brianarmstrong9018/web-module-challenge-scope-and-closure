cc4e235210b92e3f069a6826d35a7b9e0d3cf5f9
brianarmstrong9018 <brianarmstrong9018@gmail.com>
2021-04-19 16:38:25 -0500

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  Counter 2 has a global variable.
+  count variable is local scope on counter1 and global in counter2.
   2. Which of the two uses a closure? How can you tell?
-  
+  The first counter uses a closure because it invokes a function that looks for a variable outside of the inner function.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  Counter1 would be prefered in a situation where we only want the variable 
      to be accesed in the function. Counter2 would be better if the situation asks for the variable to used 
